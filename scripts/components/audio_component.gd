0fe317530fd84f1b179dd8c68397ee9de54a71ab
Ian Day <ian.day@mailbox.org>
2026-05-23 20:28:49 -0400

    Update AudioComponent

--- scripts/components/audio_component.gd
+++ scripts/components/audio_component.gd
@@ -1,11 +1,22 @@
 class_name AudioComponent
 extends Component
 
-signal audio_played
-signal audio_finished
+signal audio_started
 
 func play_audio(stream : AudioStream) -> void:
-	pass
+	var player := AudioStreamPlayer.new()
+	player.stream = stream
+	player.connect("finished", destroy_audio_player.bind(player))
+	add_child(player)
+	player.play()
 
 func play_audio3d(stream : AudioStream) -> void:
-	pass
+	var player := AudioStreamPlayer3D.new()
+	player.stream = stream
+	player.connect("finished", destroy_audio_player.bind(player))
+	add_child(player)
+	player.play()
+
+func destroy_audio_player(audio_player : Node) -> void:
+	audio_player.stop()
+	audio_player.queue_free()
