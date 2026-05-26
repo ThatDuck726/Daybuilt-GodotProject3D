d3732a5e2d96c7cd6647b9a6cd128c37e7ec1065
Ian Day <ian.day@mailbox.org>
2026-05-26 14:38:34 -0400

    Introducing MainMenu

---
+++ scenes/user_interface/main_menu/main_menu.gd
@@ -0,0 +1,21 @@
+extends MarginContainer
+
+@export var dev_world : PackedScene
+var current_container : VBoxContainer
+
+func _ready() -> void:
+	for child in %MainContainer.get_children():
+		if child is Button:
+			child.pressed.connect(_on_button_pressed.bind(child.name))
+
+func set_container(new_container : VBoxContainer) -> void:
+	pass
+
+func _on_button_pressed(button_name : String) -> void:
+	match button_name:
+		"PlayButton":
+			pass
+		"DevButton":
+			if dev_world: get_tree().change_scene_to_packed(dev_world)
+		"QuitButton":
+			get_tree().quit()
