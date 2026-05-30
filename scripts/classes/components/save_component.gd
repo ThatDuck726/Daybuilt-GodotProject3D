0b8940b57569b50f619658298f96d5c9eb431098
Ian Day <ian.day@mailbox.org>
2026-05-29 21:02:43 -0400

    Component Update

---
+++ scripts/classes/components/save_component.gd
@@ -0,0 +1,15 @@
+class_name SaveComponent
+extends Component
+
+
+func create_save(category : String, value : String) -> void:
+	pass
+
+func update_save(category : String, value : String) -> void:
+	pass
+
+func get_save(category : String) -> String:
+	return "N/A"
+
+func does_save_exist(category : String) -> bool:
+	return false
