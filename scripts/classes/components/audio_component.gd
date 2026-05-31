44c9ce633b820f5d4a8a66209e27b47bc5a1a3d4
ThatDuck726 <max.tu02006@gmail.com>
2026-05-31 15:03:18 -0400

    Update audio_component.gd

--- scripts/classes/components/audio_component.gd
+++ scripts/classes/components/audio_component.gd
@@ -12,7 +12,7 @@ func destroy_audio_player(audio_player : Node) -> void:
 # Public Methods #
 #----------------#
 
-func play_audio(stream : AudioStream, volume_linear : float) -> void:
+func play_audio(stream : AudioStream, volume_linear : float = 100) -> void:
 	var player := AudioStreamPlayer.new()
 	player.stream = stream
 	player.volume_db = linear_to_db(volume_linear)
@@ -21,11 +21,12 @@ func play_audio(stream : AudioStream, volume_linear : float) -> void:
 	add_child(player)
 	player.play()
 
-func play_audio3d(stream : AudioStream, volume_linear : float, unit_size : float) -> void:
+func play_audio3d(stream : AudioStream, volume_linear : float = 100, unit_size : float = 10.0, bus : StringName = "Master") -> void:
 	var player := AudioStreamPlayer3D.new()
 	player.stream = stream
 	player.volume_db = linear_to_db(volume_linear)
 	player.unit_size = unit_size
+	player.bus = bus
 	
 	player.connect("finished", destroy_audio_player.bind(player))
 	add_child(player)
