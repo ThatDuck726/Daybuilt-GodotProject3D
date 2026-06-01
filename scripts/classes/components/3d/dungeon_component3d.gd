268c60e8449aaf82b388bd98dd2d9fbd1871dd7b
ThatDuck726 <max.tu02006@gmail.com>
2026-06-01 00:53:01 -0400

    Update dungeon_component3d.gd

--- scripts/classes/components/3d/dungeon_component3d.gd
+++ scripts/classes/components/3d/dungeon_component3d.gd
@@ -11,7 +11,18 @@ extends Component3D
 @export var arena_rooms : Array[Node]
 
 var current_room_count : int = 0
+var current_rooms : Array[Node]
 
 func _ready() -> void:
 	if generate_on_start:
 		pass
+
+func generate_rooms() -> void:
+	for i in range(current_room_count, maximum_room_count):
+		pass
+
+func add_room() -> void:
+	pass
+
+func del_room() -> void:
+	pass
