79451dcca1ba838f1b2a7cd12d630bf6005a1901
Ian Day <ian.day@mailbox.org>
2026-06-04 13:22:45 -0400

    Introduce SteamComponent

---
+++ scripts/classes/components/steam/steam_component.gd
@@ -0,0 +1,18 @@
+@icon("res://assets/components/SteamComponentNode.svg")
+class_name SteamComponent
+extends Component
+
+var steam_id
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	if !Steam.isSteamRunning(): 
+		push_warning("Steam is not Running")
+		return
+	
+	steam_id = Steam.getSteamID()
+
+func get_steam_id() -> int:
+	if !Steam.isSteamRunning():
+		return 0
+	return steam_id
