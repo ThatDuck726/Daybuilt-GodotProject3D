335f447e7857aca42c5581c0252c37cb170fc38c
Ian Day <ian.day@mailbox.org>
2026-06-06 01:04:56 -0400

    Update Settings Container

--- scenes/user_interface/settings_menu/settings_container.gd
+++ scenes/user_interface/settings_menu/settings_container.gd
@@ -3,9 +3,25 @@ extends VBoxContainer
 @export var back_button : Button
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	for child in $HBoxContainer.get_children():
+		child.pressed.connect(_on_button_pressed.bind(child))
+	
+	set_container($GeneralContainer)
 
+func set_container(container : VBoxContainer) -> void:
+	for child in self.get_children():
+		if child is VBoxContainer:
+			child.hide()
+	container.show()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_button_pressed(button : Button) -> void:
+	var button_name : String = button.name
+	match button_name:
+		"General":
+			set_container($GeneralContainer)
+		"Controls":
+			set_container($ControlsContainer)
+		"Audio":
+			set_container($AudioContainer)
+		"Video":
+			set_container($VideoContainer)
