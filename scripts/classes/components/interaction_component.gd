337417d3a76614ab2e5f04990122b51292ec9584
ThatDuck726 <max.tu02006@gmail.com>
2026-06-07 23:13:21 -0400

    Trying my best

--- scripts/classes/components/interaction_component.gd
+++ scripts/classes/components/interaction_component.gd
@@ -1,11 +1,31 @@
 class_name InteractionComponent
 extends Component
 
+@export var is_enabled : bool = true
+@export var raycast : RayCast3D
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
+#----------------#
+# Public Methods #
+#----------------#
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+# TODO Kind of a bad implementation, but game jam is coming up so I gotta go fast
+func trigger_interaction() -> void:
+	if !is_enabled:
+		if debug: print("Interaction Disabled")
+		return
+		
+	if raycast.is_colliding():
+		var collider = raycast.get_collider()
+		if debug: print("Got Collider: ", collider)
+		
+		if !collider:
+			if debug: print("No Collider")
+			return
+		
+		for child in collider.get_children():
+			if child is InteractiveComponent:
+				child.interact()
--- scripts/classes/components/interactive_component.gd
+++ scripts/classes/components/interactive_component.gd
@@ -1,11 +1,17 @@
 class_name InteractiveComponent
 extends Component
 
+# TODO Redo this, not very good
+
+@export var interactive_callable : Callable
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
+#----------------#
+# Public Methods #
+#----------------#
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func trigger_interactive() -> void:
+	interactive_callable.call()
